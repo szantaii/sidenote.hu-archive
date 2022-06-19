0344850d8d6ab2e6d131f09e1841fc3362b364ba
szantaii <szantaii@gmail.com>
2022-06-19 14:58:28 +0200

    Extend tool to remove all unwanted HTML tags

---
+++ tools/remove-unwanted-tags/remove-unwanted-tags.php
@@ -0,0 +1,137 @@
+<?php
+
+declare(strict_types=1);
+
+function elementHasAttributesWithValues(DOMElement $element, array $attribute_name_value_pairs): bool
+{
+    $result = true;
+
+    foreach ($attribute_name_value_pairs as $attribute_name => $attribute_value)
+    {
+        $value = $element->getAttribute($attribute_name);
+
+        if (!str_contains($value, $attribute_value))
+        {
+            $result = false;
+        }
+    }
+
+    return $result;
+}
+
+function removeElements(DOMDocument $document, array $elements): DOMDocument
+{
+    $new_document       = $document;
+    $elements_to_remove = array();
+
+    foreach ($elements as $element_name => $attribute_name_value_pair_lists)
+    {
+        $selected_elements = $new_document->getElementsByTagName($element_name);
+
+        foreach ($selected_elements as $element)
+        {
+            if ($attribute_name_value_pair_lists === array())
+            {
+                $elements_to_remove[] = $element;
+
+                continue;
+            }
+
+            foreach ($attribute_name_value_pair_lists as $attribute_name_value_pairs)
+            {
+                if (elementHasAttributesWithValues($element, $attribute_name_value_pairs))
+                {
+                    $elements_to_remove[] = $element;
+
+                    continue;
+                }
+            }
+        }
+    }
+
+    foreach ($elements_to_remove as $element_to_remove)
+    {
+        $element_to_remove->parentNode->removeChild($element_to_remove);
+    }
+
+    return $new_document;
+}
+
+$file_name                    =  $argv[1];
+$file_contents                = file_get_contents($file_name);
+$document                     = new DOMDocument();
+$document->validateOnParse    = false;
+$document->resolveExternals   = false;
+$document->preserveWhiteSpace = true;
+$document->substituteEntities = false;
+$document->formatOutput       = false;
+
+$document->loadHTML(
+    mb_convert_encoding(
+        $file_contents,
+        'HTML-ENTITIES',
+        'UTF-8'
+    ),
+    LIBXML_BIGLINES | LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD | LIBXML_NONET | LIBXML_NOXMLDECL | LIBXML_PARSEHUGE
+);
+
+$elements_to_remove = array(
+    'link'   => array(
+        array(
+            'rel'  => 'alternate',
+            'type' => 'application/atom',
+        ),
+        array(
+            'rel'  => 'alternate',
+            'type' => 'application/json',
+        ),
+        array(
+            'rel'  => 'alternate',
+            'type' => 'application/rss',
+        ),
+        array(
+            'rel'  => 'alternate',
+            'type' => 'text/xml',
+        ),
+        array(
+            'rel'  => 'dns-prefetch',
+        ),
+        array(
+            'rel'  => 'EditURI',
+            'type' => 'application/rsd',
+            'href' => 'xmlrpc',
+        ),
+        array(
+            'rel'  => 'https://api.w.org/',
+            'href' => 'wp-json/index.html',
+        ),
+        array(
+            'rel'  => 'pingback',
+        ),
+        array(
+            'rel'  => 'profile',
+        ),
+        array(
+            'rel'  => 'publisher',
+        ),
+        array(
+            'rel'  => 'wlwmanifest',
+        ),
+    ),
+    'meta' => array(
+        array(
+            'name' => 'generator',
+        ),
+    ),
+    'script' => array(),
+);
+
+$document = removeElements($document, $elements_to_remove);
+
+$file_contents = mb_convert_encoding(
+    $document->saveHTML(),
+    'HTML-ENTITIES',
+    'UTF-8'
+);
+
+file_put_contents($file_name, $file_contents);
